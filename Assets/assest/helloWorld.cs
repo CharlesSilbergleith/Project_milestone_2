082496aad2934532348cd6bcc8a118a5c71b7dea
CharlesSilbergleith <csilbergleith89567@uat.edu>
2025-09-21 17:51:11 -0700

    first upload of Hello World Project
    the first creatin of the hello world project in Untiy for intro to game programing

---
+++ Assets/assest/helloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello World");
+        Debug.Log("this is an update for a branch to the hello world project");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
